b08357f87de2443cb7659c4eaa7bb0f2dfcb16d8
Шепыч <syndicating@yandex.ru>
2023-08-30 07:05:30 +0300

    feat: дизайн списка статей

--- resources/views/articles/list.blade.php
+++ resources/views/articles/list.blade.php
@@ -4,12 +4,20 @@
     <section class="articles__list">
         @foreach($articles as $article)
             <a class="article__row" href="{{ route('articles.detail', $article->slug) }}">
-                <img src="{{ $article->img }}" alt="">
+                <h2>
+                    {{ $article->title }}
+                </h2>
+                <div class="article__img__wrap">
+                    <img src="{{ $article->img }}" alt="">
+                </div>
                 <div class="article__description">
-                    <h2>{{ $article->title }}</h2>
                     <p>
-                        {{ $article->description }}
+                        {{ mb_strimwidth($article->description, 0, 170, " ...") }}
                     </p>
+                </div>
+                <div class="article__footer">
+                    <span class="article__date">27 мая</span>
+                    <span class="article__date" style="margin-right: 18px">Подробнее -></span>
                 </div>
             </a>
         @endforeach
