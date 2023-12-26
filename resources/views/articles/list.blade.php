4d12f94b15f0e4e6f46114bcd631491d97f1a52a
Шепыч <syndicating@yandex.ru>
2023-12-26 12:51:29 +0300

    feat: немного адаптации

--- resources/views/articles/list.blade.php
+++ resources/views/articles/list.blade.php
@@ -17,7 +17,8 @@
                 </div>
                 <div class="article__footer">
                     <span class="article__date" style="display: flex;justify-content: center;align-items: center; color: #262626;font-size: 20px;font-family: Roboto">ПОДРОБНЕЕ <span style="margin-left: 10px !important;box-sizing: border-box; display: block; background-image: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;"><img style="width:10px;transform: rotateY(180deg)" alt="" aria-hidden="true" src="https://page.lfl.ru/_next/static/media/buttonArrow.f548b8f7.svg" style="display: block; max-width: 100%; background-image: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;"></span></span>
-                    <span class="article__date" style="margin-right: 18px">27 мая 2023 года в 18:01</span>
+                    <span class="article__date" style="margin-right: 18px">{{ Carbon\Carbon::parse($article->created_at)->diffForHumans(); }}</span>
+                    <span class="article__date article__date__adaptive" style="margin-right: 18px">{{ Carbon\Carbon::parse($article->created_at)->format('d.m.Y') }}</span>
                 </div>
             </a>
         @endforeach
