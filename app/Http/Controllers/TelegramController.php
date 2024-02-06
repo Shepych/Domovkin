5a96235e2eaab440467b0a4ce510511622fc085e
Шепыч <syndicating@yandex.ru>
2024-02-06 18:29:13 +0300

    fix: test 9

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -94,7 +94,7 @@ class TelegramController extends Controller
             // ]);
 
             DB::table('telegram_applications')->insert([
-                'user_id' =>  123123123,
+                'user_id' =>  $result['result']['chat']['id'],
                 'description' => $response
             ]);
         }
