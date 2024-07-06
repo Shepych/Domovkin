d280662dda448256921e5c2164a7ed69cbaf59f5
shepych <syndicating@yandex.ru>
2024-07-06 09:31:46 +0300

    Fix: сортировка

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -17,24 +17,25 @@ class MainController extends Controller
     public function index() {
         $title = "Строительство домов и ремонт квартир в Москве";
         $questions = Question::all();
-        $projects = Project::take(12)->get();
+        $projects = Project::orderByRaw('ISNULL(position), position ASC')
+            ->take(12)
+            ->get();
         return view('index', compact('questions', 'projects', 'title'));
     }
 
     public function projects() {
         $title = "Проекты загородных домов и коттеджей";
-        $projects = Project::all();
+        $projects = Project::orderByRaw('ISNULL(position), position ASC')
+        ->get();
         return view('projects', compact('projects', 'title'));
     }
 
     public function project($id) {
-        
         $project = Project::find($id);
         $title = "Проект №" . $project->id;
         if(!$project) abort(404);
         return view('project', compact('project', 'title'));
     }
-    
 
     public function services() {
         $title = "Построить дом, сделать ремонт квартиры - цена (стоимость) услуги";
