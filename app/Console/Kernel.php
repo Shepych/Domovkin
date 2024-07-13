2f5d005c284b68ee0d182b1e2db6f1d682250585
shepych <syndicating@yandex.ru>
2024-07-13 10:15:47 +0300

    Fix: вернуть daily()

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('sitemap:generate')->everyMinute();
+        $schedule->command('sitemap:generate')->daily();
         // $schedule->command('inspire')->hourly();
     }
 
