25e0b37b98711b2abbd2c1c71f4cb98e4a7d9b01
shepych <syndicating@yandex.ru>
2024-07-14 14:17:24 +0300

    Fix: sitemap

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -2,8 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Article;
+use App\Models\Portfolio;
+use App\Models\Project;
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 class GenerateSitemap extends Command
 {
@@ -28,7 +33,34 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
-        SitemapGenerator::create(config('app.url'))
-            ->writeToFile(public_path('sitemap.xml'));
+        // SitemapGenerator::create(config('app.url'))
+        //     ->writeToFile(public_path('sitemap.xml'));
+
+        $sitemap = Sitemap::create();
+
+        $sitemap->add(Url::create('/'));
+        $sitemap->add(Url::create('/projects'));
+        $sitemap->add(Url::create('/articles'));
+        $sitemap->add(Url::create('/services'));
+        $sitemap->add(Url::create('/portfolio'));
+
+        $articles = Article::all();
+        foreach ($articles as $page) {
+            $sitemap->add(Url::create("/articles/{$page->slug}"));
+        }
+
+        $projects = Project::all();
+        foreach ($projects as $page) {
+            $sitemap->add(Url::create("/projects/{$page->id}"));
+        }
+
+        $portfolio = Portfolio::all();
+        foreach ($portfolio as $page) {
+            $sitemap->add(Url::create("/portfolio/{$page->id}"));
+        }
+
+        $sitemap->writeToFile(public_path('sitemap.xml'));
+
+        $this->info('Sitemap generated successfully.');
     }
 }
